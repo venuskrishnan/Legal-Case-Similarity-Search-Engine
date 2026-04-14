44fa47db3b6491bd4c5afb534d60674dd30a80b1
venuskrishnan <venuskrishnan.vk@gmail.com>
2026-04-14 13:30:58 +0530

    Add files via upload

---
+++ TACG.sh
@@ -0,0 +1,9 @@
+## To run dataset coliee2023, please change '--data 2022' to '--data 2023'
+
+python Graph_generation/TACG.py --data 2022 --dataset test --feature fact
+
+python Graph_generation/TACG.py --data 2022 --dataset test --feature issue
+
+python Graph_generation/TACG.py --data 2022 --dataset train --feature fact
+
+python Graph_generation/TACG.py --data 2022 --dataset train --feature issue
